5c274f1e3ed40597d5d3f2ead518c541e5034031
Dragan1716 <dragan.badnjar17@gmail.com>
2024-10-30 12:55:29 +0100

    App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import GenreMovieList from "./components/GenreMovieList";
 import Navbar from "./components/Navbar";
+import ProductionHouse from "./components/ProductionHouse";
+import Slider from "./components/Slider";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Slider />
+      <ProductionHouse />
+      <GenreMovieList />
     </div>
   );
 }
