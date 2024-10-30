5a2faf36c4d6e31d3c2b3b1941529144ad5a15b7
Dragan1716 <dragan.badnjar17@gmail.com>
2024-10-30 12:52:00 +0100

    Global API

---
+++ src/services/GlobalApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const movieBaseUrl = "https://api.themoviedb.org/3";
+
+const movieByGenreBaseURL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`;
+
+const getTrendingVideos = axios.get(
+  movieBaseUrl + "/trending/all/day?api_key=" + process.env.REACT_APP_API_KEY
+);
+
+const getMovieByGenreId = (id) =>
+  axios.get(movieByGenreBaseURL + "&with_genres=" + id);
+
+export default { getTrendingVideos, getMovieByGenreId };
